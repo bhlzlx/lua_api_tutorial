025c8cd1af27352ac2ab79a089c84a3bd6f0e94c
lixin <bhlzlx@gmail.com>
2021-11-18 19:25:19 +0800

    call and ret

--- samples/2_call_and_ret/main.cpp
+++ samples/2_call_and_ret/main.cpp
@@ -10,12 +10,15 @@ char const* script = R"(
 function helloworld()
     print('hello,world!')
 end
-helloworld()
+function my_pow(x,y)
+    return x^y
+end
 )";
 
 int main() {
     lua_State* state = luaL_newstate();
-    luaL_openlibs(state); {
+    luaL_openlibs(state); 
+    {
         auto rst = luaL_loadbuffer( state, script, strlen(script), "helloworld");
         if(rst != 0) {
             if(lua_isstring(state, -1)) {
@@ -31,6 +34,27 @@ int main() {
                 printf("call function chunk failed : %s\n", msg);
                 lua_pop(state, 1);
             }
+        }
+        // call my_pow
+        rst = lua_getglobal(state, "my_pow");
+        if(!lua_isfunction(state, -1)) {
+            printf("function named 'my_pow' not found!\n");
+            return -1;
+        }
+        lua_pushnumber(state, 2);
+        lua_pushnumber(state, 8);
+
+        rst = lua_pcall(state, 2, 1, 0);
+        if(rst != 0) {
+            if(lua_isstring(state, -1)) {
+                auto msg = lua_tostring(state, -1);
+                printf("call function chunk failed : %s\n", msg);
+                lua_pop(state, 1);
+            }
+        }
+        if(lua_isnumber(state, -1)) {
+            lua_Number val = lua_tonumber(state, -1);
+            printf("my_pow ret : %f", (float)val);
         }
     }
     lua_close(state);
